7ed439f1203e49338c4b595078b29267dd2b27f5
Pradeep Sahani <pradeep.sudrania@g,ail.com>
2024-03-17 15:37:08 +0530

    changes:

--- db/db.js
+++ db/db.js
@@ -14,11 +14,14 @@ var options = {
   user: DB.UserName,
   pass: DB.Password,
 };
-const MONGOURI = `mongodb://${DB.HOST}:${DB.PORT}/${DB.DATABASE}`;
+// const MONGOURI = `mongodb://${DB.HOST}:${DB.PORT}/${DB.DATABASE}`;
+
+const MONGOURI = "mongodb+srv://pradeepmeandev:K8CqVJkiYC4iL3U9@ghanadoc.sfat4gr.mongodb.net/?retryWrites=true&w=majority&appName=ghanaDoc";
+
 
 const InitiateMongoServer = async () => {
   try {
-    await mongoose.connect(MONGOURI, options);
+    await mongoose.connect(MONGOURI);
     console.log("Connected to DB !!");
   } catch (e) {
     throw e;
